f51ca09be7b84a5ea3ffdb4e3903ad90f22cd5e2
ricardo_dev <ricardo.faiaRF@gmail.com>
2021-11-10 15:48:53 +0000

    correction server

--- BestRideApi/views/views.py
+++ BestRideApi/views/views.py
@@ -21,6 +21,31 @@ env.read_env()
 
 
 import boto3
+import os
+
+class Images:
+    @api_view(['POST'])
+    def upload_file(request):
+        """
+        file_name: File to upload
+        bucket: Bucket to upload to
+        object_name: S3 object name. If not specified then file_name is used
+        return: True if file was uploaded, else False
+        """
+
+        # If S3 object_name was not specified, use file_name
+        if request.data['object_name'] is None:
+            object_name = os.path.basename(request.data['file_name'])
+
+        # Upload the file
+        s3_client = boto3.client('s3')
+
+        try:
+            response = s3_client.upload_file(request.data['file_name'], request.data['bucket'], request.data['object_name'])
+        except ClientError as e:
+            logging.error(e)
+            return False
+        return True
 
 
 
