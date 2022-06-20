ec817c96bb939648107de5a4466560c9da7af4ab
ricardo_dev <ricardo.faiaRF@gmail.com>
2022-06-20 22:28:15 +0100

    Correção bugs

--- BestRideApi/serializers.py
+++ BestRideApi/serializers.py
@@ -64,12 +64,12 @@ class RoadMapSerializer(serializers.ModelSerializer):
         geo_field = "point"
         fields = '__all__'
 
-    def create(self, validated_data):
-        pointInterests_data = validated_data.pop('pointInterest')
-        roadMap = RoadMap.objects.create(**validated_data)
-        for pointInterest in pointInterests_data:
-            PointInterest.objects.create(roadMap=roadMap, **pointInterest)
-        return roadMap
+    ##def create(self, validated_data):
+    ##    pointInterests_data = validated_data.pop('pointInterest')
+    ##    roadMap = RoadMap.objects.create(**validated_data)
+    ##    for pointInterest in pointInterests_data:
+    ##        PointInterest.objects.create(roadMap=roadMap, **pointInterest)
+    ##    return roadMap
 
 class VehicleSerializer(serializers.ModelSerializer):
 
